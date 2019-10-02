b42bcf7e64d2e67e640073d4876495ce80530b4a
yrribeiro <ynk9a@outlook.com>
2019-10-02 11:53:28 -0300

    CAlculadora com classe

---
+++ calcclasse.py
@@ -0,0 +1,16 @@
+import math
+
+def soma (a, b):
+    return a + b
+
+def subt (a, b):
+    return a - b
+
+def mult (a, b):
+    return a * b
+
+def div (a, b):
+    return a/b
+
+def pot (a, b):
+    return math.pow(a, b)
